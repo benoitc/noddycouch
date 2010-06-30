c07b8413ad107b50ba2de1f917ce36681187b748
benoitc <bchesneau@gmail.com>
2010-06-30 20:23:28 +0200

    add Consumer object.

---
+++ test_consumer.js
@@ -0,0 +1,13 @@
+var sys = require("sys"),
+    Consumer = require('./lib/noddycouch').Consumer;
+
+var c = new Consumer("http://127.0.0.1:5984/testdb");
+
+c.addListener("change", function(change) {
+  sys.puts("Got change "
+          + "seq: " + change.seq
+          + " on docid: " + change.id
+          + " rev: " + change.changes[0].rev);
+})
+
+c.wait({heartbeat: true})
