d44367961f763e009497a928143d594b04f01d39
Tim Smith <trangayesi@gmail.com>
2010-07-03 05:31:19 +0800

    Add error reporting to all calls.

--- examples/example.js
+++ examples/example.js
@@ -5,27 +5,36 @@
 var sys = require("sys"),
     fs = require("fs"),
     couchdb =require("../couchdb");
-    
-    
+
+function reportError(label) {
+  return function(error) {
+    sys.puts("ERROR: " + label + ": " + error);
+  }
+}
+
+
 s = new couchdb.Server()
 
 
 s.info({
   success: function(b) {
     sys.puts("server info:" + b.version)
-  }
+  },
+  error: reportError("server info")
 });
 
 s.uuids({
   success: function(b) {
     sys.puts("uuid:" + b.uuids[0])
-  }
+  },
+  error: reportError("uuids")
 });
 
 s.all_dbs({
   success: function(b) {
     sys.puts("all_dbs:" + b)
-  }
+  },
+  error: reportError("all_dbs")
 });
 
 s.create_db("testdb")
@@ -36,7 +45,9 @@ db.info({
   responseEncoding: "binary",
   success: function(b) {
     sys.puts("db info:" + b)
-  }
+  },
+  error: reportError("db info")
+
 });
 
 
@@ -45,9 +56,7 @@ db.saveDoc({}, {
     
     sys.puts("empty doc saved:" + doc.id);
   },
-  error: function(error) {
-    sys.puts("error:" + error)
-  }
+  error: reportError("saving empty doc")
   
 });
 
@@ -61,10 +70,12 @@ db.saveDoc(doc, {
       success: function(doc1) {
         sys.puts("test = " + doc1.test);
         sys.puts("rev: " + doc.rev + " == " + doc1._rev);
-      }
+      },
+      error: reportError("opening test doc")
     })
     
-  }
+  },
+  error: reportError("saving test doc")
 });
 
 db.allDocs({
@@ -72,7 +83,8 @@ db.allDocs({
     
     sys.puts("all docs total rows:" + docs.total_rows )
     
-  }
+  },
+  error: reportError("retrieving all docs")
 })
 
 
@@ -86,29 +98,36 @@ db.saveDoc({}, {
       success: function(res1) {
         db.fetchAttachment(docid, "test", {
           success: function(content) {
-            sys.puts("fetched attachment :" + content)
-          }
+            sys.puts("fetched attachment:" + content)
+          },
+          error: reportError("fetching hardcoded attachment")
         });
-      }
+      },
+      error: reportError("putting hardcoded attachment")
     });
-  }
+  },
+  error: reportError("saving empty doc for hardcoded attachment")
 });
 
 
-db.saveDoc({}, {
+db.saveDoc({"for": "NOTICE"}, {
   success: function(res) {
     var res = res;
-    sys.puts("notice doc saved: (" + res.id + "," +  res.rev +")");
+    sys.puts("NOTICE doc saved: (" + res.id + "," +  res.rev +")");
     var docid = res.id;
     
     db.fputAttachment(res.id, "./NOTICE", "NOTICE", {
       success: function(res1) {
+        sys.puts("NOTICE file attached");
         db.fetchAttachment(docid, "NOTICE", {
           success: function(content) {
             sys.puts("fetched NOTICE :" + content)
-          }
+          },
+          error: reportError("fetching file attachment")
         });
-      }
+      },
+      error: reportError("putting file attachment")
     });
-  }
+  },
+  error: reportError("saving empty doc for file attachment")
 });
